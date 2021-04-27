98dcf0cc77655407922c2d25e47934bbcce20b7d
tandica <williams.tandica@gmail.com>
2021-04-27 18:17:32 -0400

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+function assertEqual(actual, expected){
+  if (actual === expected){
+    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+//function that takes two arrays and returns true or false based on a perfect match
+function eqArrays(arr1, arr2){
+    if (arr1.length === arr2.length){
+      return true
+    } else {
+      return false
+    }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 7, 3], [1, 2, 26]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
