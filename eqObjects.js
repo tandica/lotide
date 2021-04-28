83ac261059858fba333ca36b1b5bc0c1deace52d
tandica <williams.tandica@gmail.com>
2021-04-28 19:53:52 -0400

    Implement eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,57 @@
+function assertEqual(actual, expected){
+  if (actual === expected){
+    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+
+function eqArrays(arr1, arr2){
+  if (arr1.length === arr2.length){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+//function should take in two objects then return true or false based on a perfect match 
+
+
+function eqObjects(object1, object2){
+  //if the length of object1 is equal to the length of object2
+  if (Object.keys(object1).length === Object.keys(object2).length){
+    //loop through object keys
+    for(let key of Object.keys(object1) ){
+      //compare both objects values for the key
+      if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+        //implement eqArrays function to support array value comparisons
+        if (!eqArrays(object1[key],object2[key])){
+          return false;       
+        }
+      }
+          
+    }
+      return true;
+  }
+  return false;    
+}
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// eqObjects(ab, ba); // => true
+// console.log(assertEqual(eqObjects(ab,ba), true));
+
+// const abc = { a: "1", b: "2", c: "3" };
+// eqObjects(ab, abc); // => false
+// console.log(assertEqual(eqObjects(ab,abc), false));
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// eqObjects(cd, dc); // => true
+// console.log(assertEqual(eqObjects(cd, dc), true));
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// eqObjects(cd, cd2); // => false
+// console.log(assertEqual(eqObjects(cd, cd2), false));
