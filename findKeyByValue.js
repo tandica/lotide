d85a323156ac118d568f14f6926930544b24fe4f
tandica <williams.tandica@gmail.com>
2021-04-28 18:52:57 -0400

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+function assertEqual(actual, expected){
+  if (actual === expected){
+    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+//function should scan the object and return the first key that contains the given value
+//if no key is found then it shoul return undefined
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+function findKeyByValue(obj, value){
+  for (let key in obj){
+    if (value === obj[key]){
+      //console.log(key)
+      return key;
+    }
+  }
+return undefined;
+
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
