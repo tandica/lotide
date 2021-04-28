4230b19e9f59d549a911065ce5bad87cab760bbb
tandica <williams.tandica@gmail.com>
2021-04-28 15:34:41 -0400

    Implement countOnly in Lotide

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+function assertEqual(actual, expected){
+  if (actual === expected){
+    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+
+function countOnly(allItems, itemsToCount){
+  let results = {};
+  
+  for (const item of allItems){
+    if (itemsToCount[item]){
+      if (results[item]){
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+    console.log(item);
+  }
+  return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
