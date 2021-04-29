b71cfd433fcaf3c81da9f84e768e4e2af82e050d
tandica <williams.tandica@gmail.com>
2021-04-29 19:18:50 -0400

    Implement findKey

---
+++ findKey.js
@@ -0,0 +1,35 @@
+function assertEqual(actual, expected){
+  if (actual === expected){
+    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function findKey(object, callback){
+  let results = Object.keys(object);
+  for (let item of results){
+    if (callback(object[item])){
+      console.log(item)
+      return item
+     }
+  }
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) )
