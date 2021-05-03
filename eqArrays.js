8209573777f96065ebc324463369ae29f1dae15e
tandica <williams.tandica@gmail.com>
2021-05-03 15:19:12 -0400

    Implement eqArrays and tail changes

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,27 @@
-function assertEqual(actual, expected){
-  if (actual === expected){
-    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const assertEqual = require('./assertEqual');
 
 //function that takes two arrays and returns true or false based on a perfect match
 function eqArrays(arr1, arr2){
     if (arr1.length === arr2.length){
       return true
-    } else {
-      return false
+    } 
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] === arr2[i]){
+        return true;
+      }
     }
+  return false
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 7, 3], [1, 2, 26]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
+module.exports = eqArrays;
+
+
+  //another solution
+// function eqArrays(arr1, arr2){
+//   if (arr1.length === arr2.length){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-// function assertEqual(actual, expected){
-//   if (actual === expected){
-//     console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// }
 const assertEqual = require('./assertEqual');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
@@ -18,16 +11,5 @@ function tail(){
  arrayWords.shift();
   
 }
-
-// tail(words);
-// assertEqual(words.length, 3);
-// assertEqual(words[0], "Lighthouse");
-// assertEqual(words[1], "Labs");
-
-// tail(array);
-// assertEqual(array.length, 1);
-
-// tail(arrayWords);
-// assertEqual(arrayWords.length, 1);
 
 module.exports = tail;
