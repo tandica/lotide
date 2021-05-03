68a8e832756b609af59bd1f79a3371018a2900e9
tandica <williams.tandica@gmail.com>
2021-05-03 14:17:59 -0400

    Implement head

--- head.js
+++ head.js
@@ -1,18 +1,5 @@
-function assertEqual(actual, expected){
-  if (actual === expected){
-    console.log(`🥭🥭🥭Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍓🍓🍓Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const assertEqual = require('./assertEqual');
 
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Coconuts and mangoes", "Coconuts and mangoes");
-// assertEqual("Coconuts and mangoes", "Bananas and strawberries");
-// assertEqual(7, 26);
-// assertEqual("Today is April 26", "Yesterday was April 25");
-// assertEqual(3.1415926535897932384626433832795, 3.1415926535897932384626433832795);
 
 function head(array){
 
@@ -21,7 +8,5 @@ function head(array){
   return array[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Goodbye", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7,6,7]), 5);
+module.exports = head;
+
