bd4efa7f54e625333ce1e1455df4b9ded45effa4
tandica <williams.tandica@gmail.com>
2021-05-03 16:29:25 -0400

    Implement eqArrays changes

--- eqArrays.js
+++ eqArrays.js
@@ -2,17 +2,16 @@ const assertEqual = require('./assertEqual');
 
 //function that takes two arrays and returns true or false based on a perfect match
 function eqArrays(arr1, arr2){
-    if (arr1.length === arr2.length){
-      return true
-    } 
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] === arr2[i]){
-        return true;
-      }
+  if (arr1.length !== arr2.length){
+    return false
+  } 
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
+      return false;
     }
-  return false
+  }
+return true
 }
-
 
 module.exports = eqArrays;
 
