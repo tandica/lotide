dd9460c9bd7bc0108292ba7543a31b4c12148395
tandica <williams.tandica@gmail.com>
2021-05-03 14:27:23 -0400

    Test for head.js

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const head = require('../head');
+
+console.log(assertEqual(head([5,6,7]), 5));
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Goodbye", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([7,6,7]), 5);
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
