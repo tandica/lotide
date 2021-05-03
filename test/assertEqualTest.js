fab9d168e76715c11e4f309895681c2f89077131
tandica <williams.tandica@gmail.com>
2021-05-03 14:28:33 -0400

    tests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Coconuts and mangoes", "Coconuts and mangoes");
+assertEqual("Coconuts and mangoes", "Bananas and strawberries");
+assertEqual(7, 26);
+assertEqual("Today is April 26", "Yesterday was April 25");
+assertEqual(3.1415926535897932384626433832795, 3.1415926535897932384626433832795);
