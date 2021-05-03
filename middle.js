2407bee74d69af6591dc3830ce71b3f8bf74680a
tandica <williams.tandica@gmail.com>
2021-05-03 16:51:24 -0400

    Implement middle changes

--- middle.js
+++ middle.js
@@ -1,22 +1,25 @@
-function eqArrays(arr1, arr2){
-  if (arr1.length === arr2.length){
-    return true
-  } else {
-    return false
-  }
-}
+// function eqArrays(arr1, arr2){
+//   if (arr1.length === arr2.length){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 
-function assertArraysEqual(arr1, arr2){
-  //implement eqArrays function
-  let outcome = eqArrays(arr1, arr2);
-  //if arrays are equal, print 'Equal'
-  if (outcome === true){
-    console.log('Equal');
-  } else {
-    //else print 'not equal'
-    console.log('Not equal');
-  }
-}
+// function assertArraysEqual(arr1, arr2){
+//   //implement eqArrays function
+//   let outcome = eqArrays(arr1, arr2);
+//   //if arrays are equal, print 'Equal'
+//   if (outcome === true){
+//     console.log('Equal');
+//   } else {
+//     //else print 'not equal'
+//     console.log('Not equal');
+//   }
+// }
+
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //the middle function should take in an array and return the middle elements of the array
 //arrays with one or two elements should return an empty array
@@ -37,8 +40,10 @@ function middle(array){
   return newArray;
 }
 
-console.log(middle([1, 2, 3, 4])); 
-console.log(middle([1, 2, 3])); 
-console.log(middle([1])); 
+// console.log(middle([1, 2, 3, 4])); 
+// console.log(middle([1, 2, 3])); 
+// console.log(middle([1])); 
 
-console.log(assertArraysEqual(middle, [1]));
+// console.log(assertArraysEqual(middle([5, 7, 6])));
+//assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+module.exports = middle;
